3610fc83ab0d4485e2cc102865fd65ece37a34e8
Karl Eriksson <iuff12345@gmail.com>
2020-10-27 15:57:23 +0100

    Added a method for checking the year

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace PNumVer
     {
         static void Main(string[] args)
         {
-
+            // bool to check if amount of of chars entered is correct or not
             bool check = false;
 
             do
@@ -21,6 +21,15 @@ namespace PNumVer
                 if (pNumber.Length == 12)
                 {
                     check = true;
+                    if (CheckYear(pNumber) && check)
+                    {
+
+                    }
+                    else
+                    {
+                        check = false;
+                        Console.WriteLine("Incorrect year! Try again");
+                    }
                 } else
                 {
                     Console.WriteLine("You entered an incorrect amount of numbers! Try again");
@@ -30,5 +39,29 @@ namespace PNumVer
             
 
         }
+
+        // Checking if year is between 1753 and 2020
+        // Returns true if this is true
+        static bool CheckYear(string pNumber)
+        {
+
+            string sYear = "";
+
+            for(int i = 0; i < 4; i++)
+            {
+                sYear = sYear + pNumber[i];
+            }
+
+            int year = int.Parse(sYear);
+
+            if(year >= 1753 && year <= 2020)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+
     }
 }
