2570067e88e6b600d8e8ffdd2fe542ac3adf7493
Karl Eriksson <iuff12345@gmail.com>
2020-10-27 16:08:51 +0100

    Added a method for checking the month

--- Program.cs
+++ Program.cs
@@ -21,9 +21,19 @@ namespace PNumVer
                 if (pNumber.Length == 12)
                 {
                     check = true;
+                    // Calling the method to check the year
                     if (CheckYear(pNumber) && check)
                     {
+                        // Calling the method to check the month
+                        if(CheckMonth(pNumber) && check)
+                        {
 
+                        }
+                        else
+                        {
+                            check = false;
+                            Console.WriteLine("Incorrect month! Try again");
+                        }
                     }
                     else
                     {
@@ -35,26 +45,42 @@ namespace PNumVer
                     Console.WriteLine("You entered an incorrect amount of numbers! Try again");
                 }
             } while (!check);
-
-            
-
         }
 
         // Checking if year is between 1753 and 2020
         // Returns true if this is true
         static bool CheckYear(string pNumber)
         {
-
             string sYear = "";
-
             for(int i = 0; i < 4; i++)
             {
                 sYear = sYear + pNumber[i];
             }
-
             int year = int.Parse(sYear);
 
             if(year >= 1753 && year <= 2020)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+
+        // Checking if month is between 1 and 12
+        // Returns true if this is true
+        static bool CheckMonth(string pNumber)
+        {
+
+            string sMonth = "";
+
+            for(int i = 4; i < 6; i++)
+            {
+                sMonth = sMonth + pNumber[i];
+            }
+            int month = int.Parse(sMonth);
+
+            if(month >= 1 && month <= 12)
             {
                 return true;
             } else
